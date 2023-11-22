ec9b2288f48f92fc2d01616fac8b6ad12e0e29d9
eshdev129 <148087160+eshdev129@users.noreply.github.com>
2023-11-22 09:20:20 -0300

    Add files via upload

---
+++ aula01/index.js
@@ -0,0 +1,77 @@
+//var nomeAluno = "João"
+
+/* let saudacao = "Bom dia!"
+
+console.log(saudacao + nomeAluno)
+------------------------------------------------
+*/
+
+//var nome = "Caixa d'água" 
+/* const resposta = `Eu trabalho próximo ${nome}`
+console.log(resposta);
+--------------------------------
+ */ 
+
+//var let eDia = true
+/* console.log(typeof eDia)
+---------------------------------
+ */
+
+//var const car = {
+    /* cor: "Branco",
+    qtdPortas: 5,
+    categoria: "Hatch",
+    nome: "Fiat Uno"
+}
+console.log(car) 
+--------------------------------
+*/
+//var const notas = [8, 7.5, 9.3, 6.5, "jaqueline"]
+
+/* console.log(notas[4]); 
+-------------------------------
+*/
+
+//varconst distanciaEmKm = 100;
+/* const precoCombustivel = 5.89;
+const KmPorLitros = 14;
+
+const litrosConsumidos =
+distanciaEmKm / KmPorLitros 
+
+const valorGasto = litrosConsumidos * precoCombustivel
+
+const resposta = `o total gasto nessa viagem sera de R${valorGasto.toFixed(2)}`
+console.log(resposta); 
+----------------------------------
+*/
+//var const numero = 17;
+
+/* const ehNumeroPar = (numero % 2) === 0;
+
+if (ehNumeroPar) {
+    console.log("O número é par");
+} else {
+    console.log("O número é impar");
+} 
+---------------------------------------- */
+
+const precoEtanol = 3.89;
+const precoGasolina = 5.89;
+const distanciaEmKm = 100;
+
+const tipoCombustivel = 'Etanol';
+var KmPorLitros = tipoCombustivel === 'Gasolina' ? 14 
+:
+
+console.log(KmPorLitros);
+
+/* if (tipoCombustivel === 'Gasolina') { 
+    const valorGasto = litrosConsumidos * precoGasolina;
+console.log(valorGasto.toFixed(2)) 
+}
+else {
+    let valorGasto = litrosConsumidos * precoEtanol;
+    console.log(valorGasto.toFixed(2)); 
+}
+ */
